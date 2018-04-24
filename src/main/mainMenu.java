f41da9a2c3555fd02ba0ab2bc717ef46d2977caa
OmarPC <o.ghrairi-15@student.lboro.ac.uk>
2018-04-24 09:41:47 +0100

    added out

---
+++ src/main/mainMenu.java
@@ -0,0 +1,11 @@
+package main;
+
+import javax.swing.*;
+
+public class mainMenu {
+    private JButton addMultAnimal;
+    private JButton addSingAnimal;
+    private JButton searchButton;
+    private JButton computeRev;
+    private JButton writeAnimals;
+}
